e4384a14b114273a40ac96cbead3b632b88c217b
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2023-12-18 14:34:50 +0000

    feat: implement WebSocket handshake

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -1,5 +1,6 @@
 module;
 
+import base64;
 import comms;
 import io_loop;
 import logger;
@@ -8,12 +9,25 @@ import sha1;
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #include <cstring>
+#include <map>
+#include <string>
 
 export module websocket;
 
 const char* web_socket_guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+class WebSocketClient {
+ public:
+  int fd;
+  char read_buffer[1024 * 8];
+  int read_buffer_len = 0;
+  char write_buffer[1024 * 8];
+  int write_buffer_len = 0;
+  bool connected = false;
+};
 
 export namespace WebSocket {
 
@@ -48,10 +62,11 @@ class Server {
     io_loop.add_handler(server_fd, IOLoop::read, [&](int fd, uint32_t events) {
       accept_connection(fd);
     });
-    logger::info("WebSocket server started on port %d\n", port);
+    logger::info("WebSocket server started on port %d", port);
   }
  private:
   IOLoop& io_loop;
+  std::map<int, WebSocketClient*> clients;
 
   void accept_connection(int fd) {
     sockaddr addr;
@@ -63,11 +78,94 @@ class Server {
     }
     char host[NI_MAXHOST];
     if (getnameinfo(&addr, addr_len, host, sizeof(host), nullptr, 0, NI_NUMERICHOST) != 0) {
-      logger::last("Failed to convert socker address to host name");
+      logger::last("Failed to convert socket address to host name");
       return;
     }
-    logger::info("[%d] new WebSocket connection from %s", client_fd, host);
-    // TODO: implement
+    logger::info("WS(%d) new connection from %s", client_fd, host);
+    WebSocketClient* client = new WebSocketClient();
+    client->fd = client_fd;
+    clients.insert({client_fd, client});
+    io_loop.add_handler(client_fd, IOLoop::read | IOLoop::edge_triggered | IOLoop::closed, [&](int fd, uint32_t events) {
+      auto iterator = clients.find(fd);
+      if (iterator == clients.end()) {
+        logger::error("WS(%d) received events %ld for not existing client", fd, events);
+      } else {
+        if (events & IOLoop::closed) {
+          disconnect(iterator->second);
+        } else {
+          if (events & IOLoop::read) {
+            handle_read(iterator->second);
+          }
+          if (events & IOLoop::write) {
+            handle_write(iterator->second);
+          }
+        }
+      }
+    });
+  }
+
+  void disconnect(WebSocketClient* client) {
+    clients.erase(client->fd);
+    io_loop.remove_handler(client->fd);
+    if (close(client->fd) == -1) {
+      logger::last("WS(%d) failed to close", client->fd);
+    } else {
+      logger::info("WS(%d) closed", client->fd);
+    }
+    delete client;
+  }
+
+  void handle_read(WebSocketClient* client) {
+    client->read_buffer_len = read(
+      client->fd,
+      client->read_buffer + client->read_buffer_len,
+      sizeof(client->read_buffer) - client->read_buffer_len);
+    if (client->read_buffer_len == -1) {
+      logger::last("WS(%d) failed to read", client->fd);
+      disconnect(client);
+      return;
+    }
+    logger::info("WS(%d) read %ld bytes", client->fd, client->read_buffer_len);
+    if (client->connected) {
+      logger::info("WS(%d) reading frames is not implemented", client->fd);
+    } else {
+      std::string_view request(client->read_buffer, client->read_buffer_len);
+      std::size_t start = request.find("Sec-WebSocket-Key");
+      if (start != std::string_view::npos) {
+        start += 19;
+        std::size_t end = start + 24;
+        strncpy(client->read_buffer + end, web_socket_guid, strlen(web_socket_guid));
+        std::string key = std::string(request.substr(start, 24 + strlen(web_socket_guid)));
+        unsigned char hash[20];
+        SHA1 checksum;
+        checksum.update(key);
+        checksum.final(hash);
+        size_t output_length;
+        char* base64_encoded = base64_encode(hash, 20, &output_length);
+        output_length = snprintf(
+            client->write_buffer, sizeof(client->write_buffer),
+            "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: "
+            "Upgrade\r\nSec-WebSocket-Accept: %s\r\n\r\n",
+            base64_encoded);
+        free(base64_encoded);
+        ssize_t written = write(client->fd, client->write_buffer, output_length);
+        if (written == -1) {
+          logger::last("WS(%d) failed to send handshake", client->fd);
+          disconnect(client);
+          return;
+        }
+        if (written != output_length) {
+          logger::error("WS(%d) incomplete handshake was send, this case is not yet implemented");
+          disconnect(client);
+          return;
+        }
+        client->connected = true;
+      }
+    }
+  }
+
+  void handle_write(WebSocketClient* client) {
+    logger::warn("WS(%d) writing is not implemented", client->fd);
   }
 };
 
