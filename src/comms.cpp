43699a141288cde0d204d632b3ba04f6d84208f5
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2023-12-19 17:18:19 +0000

    feat: add some item instances

--- src/comms.cpp
+++ src/comms.cpp
@@ -2,6 +2,7 @@ module;
 
 import logger;
 
+#include <cstring>
 #include <string>
 #include <vector>
 
@@ -11,29 +12,105 @@ export namespace Comms {
 
 enum Type {
   Group = 0,
-  Float = 1,
-  Log = 2,
-  Control = 3,
+  Log = 1,
+  Control = 2,
+  String = 3,
+  Float = 4,
 };
 
 class Item {
  public:
-  Item(int parent, std::string name, Type type)
-      : parent(parent), name(name), type(type) {
+  Item(int parent, std::string name, Type type, bool writable = false)
+      : parent(parent), name(name), type(type), writable(writable) {
   }
 
-  virtual int definition_length() = 0;
-  virtual int write_definition(char* buffer) = 0;
+  virtual int definition_length() {
+    return 0;
+  }
 
-  virtual int value_length() = 0;
-  virtual int write_value(char* buffer) = 0;
+  virtual int write_definition(char* buffer) {
+  }
 
-  virtual void on_received(char* data, int len) {}
+  virtual int value_length() {
+    return 0;
+  }
 
- private:
+  virtual int write_value(char* buffer) {
+  }
+
+  virtual void on_received(char* data, int len) {
+  }
+
+ protected:
   int parent;
   std::string name;
   Type type;
+  bool writable;
+};
+
+class ItemGroup: public Item {
+ public:
+  ItemGroup(int parent, std::string name) : Item(parent, name, Group) {
+  }
+};
+
+class ItemControl: public Item {
+ public:
+  ItemControl(int parent, std::string name) : Item(parent, name, Control) {
+    // TODO: add callback parameter
+  }
+};
+
+class ItemString: public Item {
+ public:
+  ItemString(int parent, std::string name, std::string value, bool writable = false)
+    : Item(parent, name, String, writable), value(value) {
+  }
+
+  int definition_length() {
+    return value.size();
+  }
+
+  int write_definition(char* buffer) {
+    memcpy(buffer, value.c_str(), definition_length());
+  }
+
+  int value_length() {
+    return value.size();
+  }
+
+  int write_value(char* buffer) {
+    memcpy(buffer, value.c_str(), value_length());
+  }
+
+ private:
+  std::string value;
+};
+
+class ItemFloat: public Item {
+ public:
+  ItemFloat(int parent, std::string name, double value, bool writable = false)
+    : Item(parent, name, String, writable), value(value) {
+  }
+
+  int definition_length() {
+    return sizeof(value);
+  }
+
+  int write_definition(char* buffer) {
+    memcpy(buffer, &value, definition_length());
+  }
+
+  int value_length() {
+    return sizeof(value);
+  }
+
+  int write_value(char* buffer) {
+    memcpy(buffer, &value, value_length());
+  }
+
+ private:
+  double value;
 };
 
 class Items {
