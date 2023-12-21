9904a2246f9b549d42add72dc016be2c0717812a
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2023-12-21 22:37:05 +0000

    refactor: switch to rapid logging

--- src/logger.cpp
+++ src/logger.cpp
@@ -28,9 +28,9 @@ export enum level { level_debug,
 
 class LogEntry {
  public:
-  LogEntry(std::string message) : message(message) {
+  LogEntry(char level, std::string message)
+      : level(level), message(message) {
      clock_gettime(CLOCK_REALTIME, &timestamp);
-     level = 'R';
   }
 
   void print() {
@@ -60,10 +60,6 @@ std::thread logging_thread([]() {
   }
 });
 
-export void rapid(std::string message) {
-  entries.push(new LogEntry(message));
-}
-
 static level current_level = level_debug;
 
 static std::mutex mutex;
@@ -83,19 +79,9 @@ export level get_level() {
   va_end(argptr)
 
 void log_entry(const char level, const std::string format, va_list argptr) {
-  struct timespec now_timespec;
-  clock_gettime(CLOCK_REALTIME, &now_timespec);
-  time_t now_t = now_timespec.tv_sec;
-  struct tm now_tm;
-  localtime_r(&now_t, &now_tm);
-  const std::lock_guard<std::mutex> lock(mutex);
-  fprintf(
-      stderr,
-      "%02d-%02d-%02d %02d:%02d:%02d.%'09ld %c ",
-      now_tm.tm_year + 1900, now_tm.tm_mon + 1, now_tm.tm_mday,
-      now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec,
-      now_timespec.tv_nsec, level);
-  vfprintf(stderr, (format + '\n').c_str(), argptr);
+  char message[1024];
+  int len = vsnprintf(message, sizeof(message), format.c_str(), argptr);
+  entries.push(new LogEntry(level, std::string(message, len)));
 }
 
 export void debug(const std::string format, ...) {
